beb0d523dbd8d7638fb3ae70c617610f411563e0
Mariusz Kuchta <mariusz@kuchta.dev>
2024-11-12 19:44:18 +0200

    Add sqlbs annotation for filling database with sample data

--- server/schema.sql
+++ server/schema.sql
@@ -1,29 +1,29 @@
-CREATE TABLE groups (
+CREATE TABLE groups ( --bs: low
         id INTEGER NOT NULL PRIMARY KEY,
-        name TEXT NOT NULL 
+        name TEXT NOT NULL --bs: company
 );
 
-CREATE TABLE members (
+CREATE TABLE members ( --bs: low
         id INTEGER NOT NULL PRIMARY KEY,
-        username TEXT NOT NULL,
-        displayName TEXT NOT NULL,
-        password TEXT NOT NULL 
+        username TEXT NOT NULL, --bs: username_random
+        displayName TEXT NOT NULL, --bs: fullname
+        password TEXT NOT NULL --bs: val; 1234
 );
 
-CREATE TABLE member_groups (
-        group_id INTEGER NOT NULL ,
-        member_id INTEGER NOT NULL ,
+CREATE TABLE member_groups ( --bs: medium
+        group_id INTEGER NOT NULL, --bs: rel
+        member_id INTEGER NOT NULL, --bs: rel
         PRIMARY KEY (group_id, member_id),
         FOREIGN KEY (group_id) REFERENCES groups (id),
         FOREIGN KEY (member_id) REFERENCES members (id)
 );
-CREATE TABLE items (
+CREATE TABLE items ( --bs: medium
         id INTEGER PRIMARY KEY NOT NULL,
-        timestamp INTEGER NOT NULL,
-        name TEXT NOT NULL ,
-        price REAL NOT NULL ,
-        author_id INTEGER NOT NULL ,
-        group_id INTEGER NOT NULL ,
+        timestamp INTEGER NOT NULL, --bs: timestamp_epoch
+        name TEXT NOT NULL, --bs: product
+        price REAL NOT NULL, --bs: num; 1to200
+        author_id INTEGER NOT NULL, --bs: rel
+        group_id INTEGER NOT NULL, --bs: rel
         FOREIGN KEY (group_id) REFERENCES groups (id),
         FOREIGN KEY (author_id) REFERENCES members (id)
 );
