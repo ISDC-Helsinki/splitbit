6e64bc36cec04c9a38026d7a9991adb85930d14c
rebekac <rebekacaserman@gmail.com>
2024-12-11 17:05:24 +0200

    [web] Added the Dashboard with hardcoded values
    Overview with the groups and expenses, some things are still hard coded

---
+++ web/src/routes/ExpenseCard.svelte
@@ -0,0 +1,136 @@
+<script lang="ts">
+    import Button from "$lib/buttons/Button.svelte";
+    import Icon from "$lib/misc/_icon.svelte";
+    import FamilyRestroom from "@ktibow/iconset-material-symbols/family-restroom";
+    import LocationOn from "@ktibow/iconset-material-symbols/location-on";
+    import Description from "@ktibow/iconset-material-symbols/description";
+    import Image from "@ktibow/iconset-material-symbols/image";
+    import ShoppingCart from "@ktibow/iconset-material-symbols/shopping-cart";
+    export let name: String;
+    export let debtAmount = 0; 
+    export let expense = "itemname"
+</script>
+
+<div class="card-holder">
+    <div class="left">
+        <div class="icons">
+            <Icon icon={FamilyRestroom}/>
+        </div>
+            
+        <div class="column-in-group">
+            <div>
+                In group
+            </div>
+                
+            <div>
+                <b>{name}</b>
+            </div>
+        </div>
+
+        <div class="line"></div>
+
+        <div class="centered">
+            Date
+        </div>
+
+        <div class="icons">
+            <Icon icon={ShoppingCart}/>
+        </div>
+
+        <div class="column">
+            <div>
+                {expense}
+            </div>
+
+            <div>
+                Mariusz paid {debtAmount}
+            </div>
+        </div>
+
+    </div>
+
+    <div class="right">
+        <div class="icons">
+            <Icon icon={LocationOn}/>
+        </div>
+
+        <div class="icons">
+            <Icon icon={Description}/>
+        </div>
+
+        <div class="icons">
+            <Icon icon={Image}/>
+        </div>
+
+        <div class="column">
+            <div class="red-font">
+                You lent
+            </div>
+
+            <div class="red-font">
+                {debtAmount}$
+            </div>
+        </div>
+
+        <div>
+            <Button type="tonal">View in group</Button>
+        </div>
+
+    </div>
+</div>
+
+<style>
+    .card-holder {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .left {
+        display: flex;
+        flex-direction: row;
+        gap: 20px;
+    }
+
+    .right {
+        display: flex;
+        flex-direction: row;
+        justify-items: flex-end;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .column {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .column-in-group {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100px;
+    }
+
+    .icons {
+        display: flex;
+        align-items: center;
+        font-size: 30px;
+    }
+
+    .centered {
+        display: flex;
+        align-items: center;
+    }
+
+    .line {
+        border-left: 2px solid darkslategrey;
+        height: 100%;
+        border-radius: 1%;
+    }
+
+    .red-font {
+        color: rgb(250, 18, 18);
+    }
+</style>
---
+++ web/src/routes/LongGroupCard.svelte
@@ -0,0 +1,146 @@
+<script lang="ts">
+    import Button from "$lib/buttons/Button.svelte";
+    import Icon from "$lib/misc/_icon.svelte";
+    import FamilyRestroom from "@ktibow/iconset-material-symbols/family-restroom";
+    import Unarchive from "@ktibow/iconset-material-symbols/unarchive"
+    import SyncAlt from "@ktibow/iconset-material-symbols/sync-alt"
+    import Group from "@ktibow/iconset-material-symbols/group"
+    export let name: String;
+    export let debtAmount = 0; 
+    export let id;
+    export let date;
+    
+    function handleUnarchive() {
+        
+        const confirmation = window.confirm(`Do you really want to unarchive ${name}?`);
+        if (confirmation) {
+            console.log(`Group {id} unarchived.`);
+        } else {
+            console.log("Unarchiving canceled.");
+        }
+
+    }
+</script>
+
+<div class="card">
+    <div class="left">
+        <div>
+            <div class="row">
+                <div>
+                    <Icon icon={FamilyRestroom} class="icon-group-card"/>
+                </div>
+
+                <div>
+                    <div>
+                        Archived on {date}
+                    </div>
+
+                    <div class="name">
+                        {name}
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="row">
+            <div>
+                <Icon icon={SyncAlt}/>
+            </div>
+
+            <div class="debt">
+                You owe: ${debtAmount}
+            </div>
+        </div>
+
+        <div class="row">
+            <div>
+                <Icon icon={Group}/>
+            </div>
+
+            <div class="debt">
+                3 members: Member1, Member2, Member3
+            </div>
+        </div>
+    </div>
+
+    
+
+    <div class="right">
+        <div>
+            <Button type="outlined" on:click={handleUnarchive}>
+                <div class="row">
+                    <div>
+                        <Icon icon = {Unarchive}/>
+                    </div> 
+
+                    <div>
+                        Unarchive
+                    </div>
+                </div>
+            </Button>
+        </div>
+        
+        <div>
+            <a href="g/{id}"><Button type="filled">View</Button></a>
+        </div>
+    </div>
+</div>
+
+<style>
+    .card {
+        width: 35vw;
+        height: 30%; 
+        border-radius: 8px;
+        overflow: hidden;
+        display: flex;
+        flex-direction: row;
+        background: linear-gradient(to left, rgba(0, 0, 0, 0) -10%, rgba(10, 10, 10, 10) 100%), url("/src/poolcover.jpg");
+        background-size: cover;
+        background-position: center;
+        justify-content: space-between;
+        padding: 7px;
+        color: whitesmoke;
+    }
+    
+    .left {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+:global(.icon-group-card) {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    padding: 8px;
+    font-size: 24px;
+    color: rgb(41, 51, 51);
+    }
+
+    .name {
+        font-size: 1.3rem; 
+        font-weight: bold; 
+        margin: 0;
+    }
+
+    .debt {
+        padding: 0px;
+    }
+
+    .right {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 7px;
+    }
+
+    .row {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 10px;
+    }
+
+</style>
